9e2693412ea0dab1431ae8c9da2987a62d73f777
Akash Raj <akashraj@Akashs-MacBook-Air.local>
2026-08-18 22:49:43 +0530

    restored c files

---
+++ day2_Q4.c
@@ -0,0 +1,15 @@
+//Q4: Write a program to calculate the area and circumference of a circle given its radius.
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double radius;
+    scanf("%lf", &radius);
+
+    double area = M_PI * pow(radius, 2);
+    double circumference = 2 * M_PI * radius;
+
+    printf("Area=%.2f, Circumference=%.2f\n", area, circumference);
+
+    return 0;
+}
