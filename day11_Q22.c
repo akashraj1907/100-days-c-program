714f08ffa5cc0f09ba9a74f812d1e6d36b350f4b
Akash Raj <akash8294raj@gmail.com>
2026-08-20 16:24:18 +0530

    Day 11 codes added

---
+++ day11_Q22.c
@@ -0,0 +1,27 @@
+//Q22: Write a program to find profit or loss percentage given cost price and selling price.
+#include <stdio.h>
+
+int main() {
+    double cp, sp, difference, percentage;
+
+    scanf("%lf %lf", &cp, &sp);
+
+    if (sp > cp) {
+
+        difference = sp - cp;
+        percentage = (difference / cp) * 100.0;
+        printf("Profit %.0f%%\n", percentage);
+    } 
+    else if (cp > sp) {
+ 
+        difference = cp - sp;
+        percentage = (difference / cp) * 100.0;
+        printf("Loss %.0f%%\n", percentage);
+    } 
+    else {
+ 
+        printf("No Profit No Loss\n");
+    }
+
+    return 0;
+}
