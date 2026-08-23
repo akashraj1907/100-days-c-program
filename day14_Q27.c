41f7b38b8aa3c0a817acccbffe67e12c839cb330
Akash Raj <akash8294raj@gmail.com>
2026-08-23 21:19:54 +0530

    Added day14_Q27.c and day14_Q28.c code

---
+++ day14_Q27.c
@@ -0,0 +1,17 @@
+//program in c- Q27: Write a program to print the sum of the first n odd numbers.
+#include <stdio.h>
+
+int main() {
+    int n, sum = 0;
+
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        sum += (2 * i - 1);
+    }
+    
+    // Print the result
+    printf("%d\n", sum);
+    
+    return 0;
+}
