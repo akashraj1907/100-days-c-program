02990155ed0dbf9bbda3dba20ae4f0524da99c25
akashraj1907 <akash8294raj@gmail.com>
2026-08-28 14:13:29 +0530

    Added day19 Q38 code

---
+++ day19_Q38.c
@@ -0,0 +1,20 @@
+//Q38: Write a program to find the sum of digits of a number.
+#include <stdio.h>
+
+int main() {
+    int n, sum = 0;
+
+    printf("Enter a number: ");
+
+    if (scanf("%d", &n) == 1) {
+
+        if (n < 0) n = -n;
+        
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        printf("%d\n", sum);
+    }
+    return 0;
+}
