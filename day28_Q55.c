f78d3402ebc0dbc653f1dacc15d445faab659cdd
akashraj1907 <akash8294raj@gmail.com>
2026-09-06 18:14:36 +0530

    Added day28 Q55 code

---
+++ day28_Q55.c
@@ -0,0 +1,18 @@
+//Q55: Write a program to print all the prime numbers from 1 to n.
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter the number(n): ");
+
+    if (scanf("%d", &n) != 1) return 0;
+    
+    for (int i = 2; i <= n; i++) {
+        int prime = 1;
+        for (int j = 2; j * j <= i; j++) {
+            if (i % j == 0) { prime = 0; break; }
+        }
+        if (prime) printf("%d ", i);
+    }
+    return 0;
+}
