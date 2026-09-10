0b1f893b7704edaf2e1885232da4b3e346098869
akashraj1907 <akash8294raj@gmail.com>
2026-09-10 11:15:21 +0530

    Added day32 Q63 code

---
+++ day32_Q63.c
@@ -0,0 +1,18 @@
+//Q63: Merge two arrays.
+#include <stdio.h>
+
+int main() {
+    int n1, n2;
+    scanf("%d", &n1);
+    int a[n1];
+    for(int i = 0; i < n1; i++) scanf("%d", &a[i]);
+    
+    scanf("%d", &n2);
+    int b[n2];
+    for(int i = 0; i < n2; i++) scanf("%d", &b[i]);
+    
+    for(int i = 0; i < n1; i++) printf("%d ", a[i]);
+    for(int i = 0; i < n2; i++) printf("%d%s", b[i], (i == n2 - 1) ? "" : " ");
+    
+    return 0;
+}
