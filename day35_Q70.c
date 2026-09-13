49d98052ba7a603d5900c67f0946ddf78596bda9
akashraj1907 <akash8294raj@gmail.com>
2026-09-13 10:05:24 +0530

    Added day35 Q70 code

---
+++ day35_Q70.c
@@ -0,0 +1,42 @@
+//Q70: Rotate an array to the right by k positions.
+#include <stdio.h>
+
+void reverse(int arr[], int start, int end) {
+    while (start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+void rotateRight(int arr[], int n, int k) {
+    k = k % n;
+
+    reverse(arr, 0, n - 1);
+
+    reverse(arr, 0, k - 1);
+
+    reverse(arr, k, n - 1);
+}
+
+int main() {
+    int n, k;
+    if (scanf("%d", &n) != 1 || n <= 0) return 0;
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    scanf("%d", &k);
+
+    rotateRight(arr, n, k);
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
