1b7c2e00aa8c3f41703800e6131e177a5ace5e31
akashraj1907 <akash8294raj@gmail.com>
2026-09-14 09:51:39 +0530

    Added day36 Q71 code

---
+++ day36_Q71.c
@@ -0,0 +1,27 @@
+//Q71: Read and print a matrix.
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+
+    if (scanf("%d %d", &rows, &cols) != 2) {
+        return 1;
+    }
+
+    int matrix[rows][cols];
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
