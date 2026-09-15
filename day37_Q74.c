a03802201ec8d8ba1a579ae86cedd859daeb975c
akashraj1907 <akash8294raj@gmail.com>
2026-09-15 18:18:37 +0530

    Added day37 Q74 code

---
+++ day37_Q74.c
@@ -0,0 +1,30 @@
+//Q74: Find the transpose of a matrix.
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+
+    if (scanf("%d %d", &rows, &cols) != 2) {
+        return 0;
+    }
+    
+    int matrix[rows][cols];
+ 
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    for (int j = 0; j < cols; j++) {
+        for (int i = 0; i < rows; i++) {
+            printf("%d", matrix[i][j]);
+            if (i < rows - 1) {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
